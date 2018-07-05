0f0bd7eff71651af47015ffb34202c08f04b58ba
Xiao (Frank) Yang <xiaoyang.code4fun@gmail.com>
2018-07-05 12:27:09 -0400

    Add include file

---
+++ include/apue.h
@@ -0,0 +1,44 @@
+#ifndef _APUE_H
+#define _APUE_H
+
+#define _POSIX_C_SOURCE 200809L
+
+#if defined(SOLARIS) /* Solaris 10 */
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 700
+#endif
+
+#include <sys/types.h> /* some systems still require this */
+#include <sys/stat.h>
+#include <sys/termios.h> /* for winsize */
+#if defined(MACOS) || !defined(TIOCGWINSZ)
+#include <sys/ioctl.h>
+#endif
+
+#include <stdio.h> /* for convenience */
+#include <stdlib.h> /* for convenience */
+#include <stddef.h> /* for offsetof */
+#include <string.h> /* for convenience */
+#include <unistd.h> /* for convenience */
+#include <signal.h> /* for SIG_ERR */
+
+#define MAXLINE 4096 /* max line length */
+
+/*
+ * Default file access permissions for new files.
+ */
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+/*
+ * Default permissions for new directories.
+ */
+#define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+
+typedef void Sigfunc(int); /* for signal handlers */
+
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+#endif /* _APUE_H */
+
